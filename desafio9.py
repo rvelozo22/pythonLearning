62866b2fff77f9bb2acee39e7458b9ba233636d8
rvelozo22 <renatovelozo22@gmail.com>
2025-06-15 11:55:45 -0300

    Desafio importação de módulo + cálculo da hipotenusa

--- desafio9.py
+++ desafio9.py
@@ -1,10 +1,11 @@
 index = [1,2,3,4,5,6,7,8,9,10]
-in1 = int(input('Digite um número inteiro:'))
+in1 = int(input('Digite um número inteiro: '))
 
-print('A tabuada de {} é:'.format(in1))
+print('\nA tabuada de {} é:'.format(in1))
 
+print("-"*13)
 for i in index:
     result = in1*i
-    print(f"{in1} x {i} = {result}")
-
+    print(f"{in1} x {i:2} = {result}")
+print("-"*13)
 
