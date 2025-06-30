7c0e3cbb3abb6eb1d35623ab2da2fec3d138b3bb
rvelozo22 <renatovelozo22@gmail.com>
2025-06-30 11:50:01 -0300

    Verifica se número digitado é par ou impar

---
+++ desafio30.py
@@ -0,0 +1,7 @@
+num = int((input('Digite um número e te direi se ele é par ou impar: ')))
+
+if num%2 == 0:
+    print(f'Você digitou {num} que é um número PAR!')
+else:
+    print(f'O número {num} é ÍMPAR!')
+
