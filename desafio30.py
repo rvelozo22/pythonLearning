b1bfa38ed5f14428a2f0b136e5c25cd94d2164c6
rvelozo22 <renatovelozo22@gmail.com>
2025-06-30 14:14:44 -0300

    Adicionando  cores ao terminal

--- desafio30.py
+++ desafio30.py
@@ -1,7 +1,7 @@
 num = int((input('Digite um número e te direi se ele é par ou impar: ')))
 
 if num%2 == 0:
-    print(f'Você digitou {num} que é um número PAR!')
+    print(f'Você digitou {num} que é um número \033[1;34mPAR!\033[m')
 else:
-    print(f'O número {num} é ÍMPAR!')
+    print(f'O número {num} é \033[1;34mÍMPAR \033[m!')
 
