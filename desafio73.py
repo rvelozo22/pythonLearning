ff600a6f75454af67f8949eaa51a6f26acafce07
rvelozo22 <renatovelozo22@gmail.com>
2025-07-12 12:55:38 -0300

    Mostra os times do brasileirão 2025: a) Classificação geral, b) Os 4 primeiros colocados, c) Os 4 últimos colocaods, d) Os times em ordem alfabética

---
+++ desafio73.py
@@ -0,0 +1,10 @@
+times = ('Flamengo', 'Cruzeiro', 'Bragantino', 'Palmeiras',
+                 'Bahia', 'Fluminense', 'Atlético-MG', 'Botafogo',
+                 'Mirassol', 'Corinthians', 'Grêmio',
+                 'Ceará', 'Vasco', 'São Paulo', 'Santos', 'Vitória',
+                 'Internacional', 'Fortaleza', 'Juventude', 'Sport')
+
+print(f'Times dos Brasileirão: {times}')
+print(f'\nOs 4 primeiros colocados são: {times[:5]}')
+print(f'\nOs 4 últimos colocados são: {times[-4:]}')
+print(f'\nTimes em ordem alfabética: {sorted(times)}')
