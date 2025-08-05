453fdfcd80c898de813892a528fa5c4618b7444f
rvelozo22 <renatovelozo22@gmail.com>
2025-08-05 18:25:18 -0300

    Cálculo da área de um terreno usando função

---
+++ desafio96.py
@@ -0,0 +1,7 @@
+def area(a,b):
+    print(f'A área do terreno é: {a*b:.1f}m²')
+
+largura = float(input('Largura (m): '))
+comprimento = float(input('Comprimento (m): '))
+
+area(largura, comprimento)
